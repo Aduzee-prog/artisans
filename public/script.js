ffaf80ccb24a454b54310d5c68bb1dc12d80e3b4
Aduzee-prog <zeenatayomiposi@gmail.com>
2025-10-19 13:02:41 +0100

    project

---
+++ public/script.js
@@ -0,0 +1,10 @@
+const registerCustomer = () => {
+            setTimeout(()=>{
+                window.location.href = "customer.html"
+            },1500)
+        }
+        const registerUser = () => {
+           setTimeout(()=>{
+                window.location.href = "user.html"
+            },1500) 
+        }
